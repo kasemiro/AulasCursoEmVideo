fbc0f456d8621f2a3562ee11354113612989d5ef
KASEMIRO <52922642+kasemiro@users.noreply.github.com>
2020-05-13 21:38:45 -0300

    Exercicios Curso em Video

---
+++ Mundo1/ex024.py
@@ -0,0 +1,3 @@
+#Saber se o nome da cidade começa com Santo
+city = str(input('Qual é a sua cidade: ')).strip()
+print('Santo' in city.title())
---
+++ Mundo1/ex025.py
@@ -0,0 +1,2 @@
+nome = str(input('Qual o seu nome? ')).strip()
+print('Silva' in nome.title())
